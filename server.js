5ff4c6947cff6897c06d5ad9d0c73272c1aa4664
aleem ahmed <aleem.ahmed1997@gmail.com>
2020-09-08 11:17:53 -0400

    Twitter NPM

--- server.js
+++ server.js
@@ -5,7 +5,25 @@
 */
 // [REQUIRE] //
 const mongoose = require('mongoose')
+const Twitter = require('twitter')
 require('dotenv').config()
+
+
+// [TWITTER] //
+var client = new Twitter({
+	consumer_key: process.env.CONSUMER_KEY || '',
+	consumer_secret: process.env.CONSUMER_SECRET || '',
+	access_token_key: process.env.ACCESS_TOKEN_KEY || '',
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET || ''
+})
+  
+var params = { screen_name: 'nodejs' }
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	// If No Error //
+	if (!error) { console.log(tweets) }
+})
+
 
 // [MONGOOSE-CONNECT] //
 mongoose.connect(
